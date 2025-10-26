7501aba38bae189abbb0bc4b0b878e83a66ad8da
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-25 20:19:03 -0600

    feat(models): Add models: pizza, topping

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Pizza(models.Model):
+    """A pizza to order."""
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name.title()
+
+class Topping(models.Model):
+    """Toppings for a pizza."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+
+    def __str__(self):
+        return f"Toppings: {self.name}"
--- pizzeria_project/settings.py
+++ pizzeria_project/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'pizzas',
+
+    # Django's default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
